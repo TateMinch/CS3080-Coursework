47afd87f66b0e6b453d5abea1d26f3a8865d8433
Tate Minch <tminch@uccs.edu>
2023-01-30 15:42:09 -0700

    Adding full functionality for bash script

--- hw2/programCounter.sh
+++ hw2/programCounter.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+loseCount=0
+
 for i in {1..10000}
 do
-    python3 hw2_tate_minch_ex_4_3.py | grep 'Sorry, the number I was thinking'
+out=$(python3 hw2_tate_minch_ex_4_3.py | grep 'Sorry, the number I was thinking')
+    if [ -n "$out"  ];
+    then
+        ((loseCount=loseCount+1))
+    fi
 done
+echo $loseCount
 
 
